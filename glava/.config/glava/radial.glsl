0da5f141eba686a929f0c32727ac00d8ffee2fd8
aittaleb <aittaleb@insa-toulouse.fr>
2023-07-27 00:00:35 +0100

    add nvim config

--- glava/.config/glava/radial.glsl
+++ glava/.config/glava/radial.glsl
@@ -4,19 +4,19 @@
 /* center line thickness (pixels) */
 #define C_LINE 12
 /* outline color */
-#define OUTLINE #E2BA73
+#define OUTLINE #A93F84
 /* number of bars (use even values for best results) */
 #define NBARS 80
 /* width (in pixels) of each bar*/
 #define BAR_WIDTH 14
 /* outline color */
-#define BAR_OUTLINE #E2BA73
+#define BAR_OUTLINE #A93F84
 /* outline width (in pixels, set to 0 to disable outline drawing) */
-#define BAR_OUTLINE #E2BA73
+#define BAR_OUTLINE #A93F84
 /* Amplify magnitude of the results each bar displays */
 #define AMPLIFY 600
 /* Bar color */ 
-#define COLOR (#8E4657 * ((d / 40) + 1))
+#define COLOR (#B53F79 * ((d / 40) + 1))
 /* Angle (in radians) for how much to rotate the visualizer */
 #define ROTATE (PI / 2)
 /* Whether to switch left/right audio buffers */
